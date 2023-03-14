644995335b1263460b674862085524a04096c679
Deshmukh P <deshmukhpurushothaman@gmail.com>
2023-03-14 08:45:37 +0530

    🗃️connection established to db

--- golang_react/server/cmd/api/main.go
+++ golang_react/server/cmd/api/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/deshmukhpurushothaman/golang-snippets/golang_react/server/pkg/routes"
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -15,6 +19,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 type AppStatus struct {
@@ -28,6 +35,7 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 5000, "Server to listen on")
 	flag.StringVar(&cfg.env, "env", "development", "Application environment")
+	flag.StringVar(&cfg.db.dsn, "dsn", "postgres://deshmukh:deshmukh@localhost/go_movies?sslmode=disable", "Postgres connection string")
 	flag.Parse()
 
 	fmt.Println("Running")
@@ -50,13 +58,35 @@ func main() {
 	// 	w.Write(js)
 	// })
 
+	db, err := openDB(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	r := mux.NewRouter()
 	routes.Routes(r)
 	routes.MovieRoutes(r)
 	http.Handle("/", r)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.port), r)
+	err = http.ListenAndServe(fmt.Sprintf(":%d", cfg.port), r)
 
 	if err != nil {
 		log.Println(err)
 	}
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
--- golang_react/server/go.mod
+++ golang_react/server/go.mod
@@ -8,4 +8,5 @@ require (
 	github.com/go-sql-driver/mysql v1.5.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/lib/pq v1.10.7
 )
