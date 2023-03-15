d787edce981fa91a4acdd7022cf7d44294b3f061
Deshmukh P <deshmukhpurushothaman@gmail.com>
2023-03-15 09:52:26 +0530

    misc update

--- golang_react/server/pkg/routes/movie.routes.go
+++ golang_react/server/pkg/routes/movie.routes.go
@@ -2,18 +2,8 @@ package routes
 
 import (
 	"github.com/deshmukhpurushothaman/golang-snippets/golang_react/server/pkg/controllers"
-	"github.com/gorilla/mux"
-
 	// "github.com/deshmukhpurushothaman/golang-snippets/golang_react/server/cmd/api/main"
-
-	"github.com/deshmukhpurushothaman/golang-snippets/golang_react/server/pkg/models"
 )
-
-type application struct {
-	config struct{}
-	router *mux.Router
-	models models.Models
-}
 
 func (app *application) MovieRoutes() {
 	app.router.HandleFunc("/v1/movie/{movieId}", controllers.GetOneMovie).Methods("GET")
